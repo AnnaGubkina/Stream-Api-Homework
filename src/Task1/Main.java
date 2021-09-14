7dd7869088b500da7defb87991b27f30c450102b
AnnaGubkina <gubkinatour@yandex>
2021-09-14 22:40:36 +0300

    files added

---
+++ src/Task1/Main.java
@@ -0,0 +1,38 @@
+package Task1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int[] intlist = {1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4};
+
+        //можно использовать Arrays.sort(intlist), но мы сортируем вручную
+        System.out.println("Положительные,четные числа в массиве, отсортированные по возрастанию: ");
+        for (int i = 0; i < intlist.length; i++) {
+            if (intlist[i] > 0 && intlist[i] % 2 == 0) {
+                System.out.println(intlist[i]);
+            }
+            //сортируем
+            for (int j = intlist.length - 1; j > 0; j--) {
+                for (int с = 0; с < j; с++) {
+                /*Сравниваем элементы попарно,
+              если они имеют неправильный порядок,
+              то меняем местами
+                 */
+                    if (intlist[с] > intlist[с + 1]) {
+                        int tmp = intlist[с];
+                        intlist[с] = intlist[с + 1];
+                        intlist[с + 1] = tmp;
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ src/population_census_task2/Main.java
@@ -0,0 +1,59 @@
+package population_census_task2;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+
+        //определяем несовершеннолетних
+        long underage =
+                persons.stream()
+                        .filter(n -> n.getAge() < 18)
+                        .count();
+        System.out.println(underage);
+
+        System.out.println();
+
+        //определяем список фамилий призывников (т.е. мужчин от 18 и до 27 лет).
+        List<String> armyAge = persons.stream()
+                .filter(n -> n.getAge() >= 18)
+                .filter(n -> n.getAge() < 27)
+                .filter(n -> n.getSex() == Sex.MAN)
+                .map(p -> p.getFamily())
+                .collect(Collectors.toList());
+        System.out.println(armyAge);
+
+//        for (String s : armyAge) {
+//            System.out.println(s);
+//        }
+
+        System.out.println();
+        //получаем отсортированный по фамилии список потенциально работоспособных людей с высшим образованием
+        List<Person> workablePeople = persons.stream()
+                .filter(n -> n.getEducation() == Education.HIGHER)
+                .filter(n -> n.getAge() >= 18)
+                .filter((t) -> (t.getSex().equals(Sex.WOMAN) && t.getAge() < 60) || (t.getSex().equals(Sex.MAN)
+                        && t.getAge() < 65))
+                .sorted(Comparator.comparing(Person::getFamily))
+                .collect(Collectors.toList());
+        System.out.println(workablePeople);
+
+
+    }
+}
